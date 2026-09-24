0790981af74fea38f96347a006e19f5b62a7af9e
Keiner Hurtado <keinerhurtado2805@gmail.com>
2026-09-24 13:15:00 +0200

    queda el ultimo

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -34,7 +34,7 @@ class Plant:
         return self._height
 
     def get_name(self) -> str:
-        return self._name.capitalize()
+        return self._name
 
     def set_height(self, new_height: float) -> None:
         if new_height > 0:
@@ -59,18 +59,80 @@ class Plant:
 
 
 class Flower(Plant):
+    is_bloomed = False
     def __init__(self, name: str, height: float, days: int, color: str) -> None:
         super().__init__(name, height, days)
+        self.color = color
 
     def show(self) -> None:
         super().show()
+        print(" Color:", self.color)
+        if self.is_bloomed:
+            print("", self.get_name().capitalize(), "is blooming beautifully!")
+        else:
+            print("", self.get_name().capitalize(), "has not bloomed yet")
+    
+    def bloom(self) -> None:
+        print("[asking the", self.get_name(), "to bloom]")
+        self.is_bloomed = True
 
+
+class Tree(Plant):
+    def __init__(self, name: str, height: float, days: int, trunk_diameter: float) -> None:
+        super().__init__(name, height, days)
+        self.trunk_diameter = trunk_diameter
+
+    def show(self) -> None:
+        super().show()
+        print(" Trunk diameter: ", self.trunk_diameter, "cm", sep="")
+        
+    def produce_shade(self) -> None:
+        print("[asking the", self.get_name(), "to produce shade]")
+        print("Tree ", self.get_name().capitalize(),
+              " now produces a shade of ",
+              self.get_height(), "cm long and ",
+              self.trunk_diameter, "cm wide.", sep="")
+
+
+class Vegetable(Plant):
+    nutritional_value = 0
+    def __init__(self, name: str, height: float, days: int, harvest_season: str) -> None:
+            super().__init__(name, height, days)
+            self.harvest_season = harvest_season
+
+    def show(self) -> None:
+        super().show()
+        print(" Harvest season:", self.harvest_season.capitalize())
+        print(" Nutritional value:", round(self.nutritional_value))
+
+    def age(self) -> None:
+        self._days = self.get_age() + 1
+        self.nutritional_value += 0.5
+
+    def grow(self) -> None:
+        size_increment = round(self.get_height() / self.get_age(), 1)
+        self._height = self.get_height() + size_increment
+        self.nutritional_value += 0.5
 
 def main() -> None:
     print("=== Garden Plant Types ===")
     print("=== Flower")
-    p1 = Flower("rose", 15, 10, "red")
+    p1 = Flower("rose", 15.0, 10, "red")
     p1.show()
+    p1.bloom()
+    p1.show()
+    print("\n=== Tree")
+    p2 = Tree("oak", 200.0, 365, 5.0)
+    p2.show()
+    p2.produce_shade()
+    print("\n=== Vegetable")
+    p3 = Vegetable("tomato", 5.0, 10, "april")
+    p3.show()
+    print("[make tomato grow and age for 20 days]")
+    for n in range(0, 20):
+        p3.age()
+        p3.grow()
+    p3.show()
 
 if __name__ == "__main__":
     main()
